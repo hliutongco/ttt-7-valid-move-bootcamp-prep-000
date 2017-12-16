651bd74507e90564531633d8c75e89bc83538369
Helen Liutongco <bellpickle@gmail.com>
2017-12-16 23:06:35 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,9 +1,18 @@
 # code your #valid_move? method here
-def valid_move
-
+def valid_move(board,index)
+  if position_taken?(board,index) && within_board?(index)
+    true
+  else
+    false
+  end
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board,index)
   (board[index] == " " || board[index] == "" || board[index] == nil) ? false : true
+end
+
+def within_board?(index)
+  index=index.to_i
+  index.between?(0,8)
 end
