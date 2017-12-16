363c1207c2ef5c52d5e0cfa349d309cf5d172eda
Helen Liutongco <bellpickle@gmail.com>
2017-12-16 23:16:18 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -12,7 +12,7 @@ end
 def valid_move?(board,index)
   position = position_taken?(board,index)
   within_board=within_board?(index)
-  if within_board && !position
+  if within_board?(index) && !position
     true
   else
     false
